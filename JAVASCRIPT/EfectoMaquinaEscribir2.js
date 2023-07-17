73feb95deae8f555683918a0fd4f1fc846c869d3
LuisLo25 <94856298+LuisLo25@users.noreply.github.com>
2023-07-16 22:48:50 -0500

    actualizacion v1.2

--- JAVASCRIPT/EfectoMaquinaEscribir2.js
+++ JAVASCRIPT/EfectoMaquinaEscribir2.js
@@ -1,5 +1,5 @@
-var typed=new Typed(".AcercadeMiCard-Container-TextDinamico",{
-    strings:["Desarrollador","Bachiller","Proactivo","Analitico"],
+var typed=new Typed(".Presentacion-Card-Descripcion-Automatico",{
+    strings:["Programar","Trabajar en Equipo","Crecer Profesionalmente"],
     typeSpeed:150,
     backSpeed:150,
 loop:true
