febd1f5f578e4ff62a8274fb6bcb479af2701e4a
LuisLo25 <94856298+LuisLo25@users.noreply.github.com>
2023-07-24 17:53:40 -0500

    Version 1.9

--- JAVASCRIPT/AnimacionMovil.js
+++ JAVASCRIPT/AnimacionMovil.js
@@ -4,22 +4,13 @@ function changeClassOnResponsive() {
 
     // Utilizar .toggle() para alternar entre las clases
     if (viewportWidth <= 750) {
-        sectionElement.classList.toggle("Seccion1", false)
-        sectionElement.classList.toggle("Sec1-movil", true)
-        sectionElement.classList.toggle("Seccion2", false)
-        sectionElement.classList.toggle("Sec2-movil", true)
-        sectionElement.classList.toggle("Seccion3", false)
-        sectionElement.classList.toggle("Sec3-movil", true)
-    } else {
-        sectionElement.classList.toggle("Seccion1", true)
-        sectionElement.classList.toggle("Sec1-movil", false)
-        sectionElement.classList.toggle("Seccion2", true)
-        sectionElement.classList.toggle("Sec2-movil", false)
-        sectionElement.classList.toggle("Seccion3", true)
-        sectionElement.classList.toggle("Sec3-movil", false)
-    }
+       
+        ScrollReveal().reveal(".Seccion2",{ desktop:false})
+        
+      
+    } 
 }
 
 // Llamar a la función cuando se carga la página y cuando cambia el tamaño de la ventana
 window.addEventListener("load", changeClassOnResponsive)
-window.addEventListener("resize", changeClassOnResponsive)
+//window.addEventListener("resize", changeClassOnResponsive)
--- JAVASCRIPT/AnimacionScroll.js
+++ JAVASCRIPT/AnimacionScroll.js
@@ -4,28 +4,28 @@ sr.reveal('.NavBar', {
     duration: 4000,
     origin: 'bottom',
     distance: '-20px',
-    reset: "true",
-    mobile: "false"
+    reset: true,
+    mobile: false
 });
 sr.reveal('.Seccion1', {
     duration: 4000,
     origin: 'bottom',
     distance: '-20px',
     reset: "true",
-    mobile: "false"
+    mobile: false
 });
 sr.reveal('.Seccion2', {
     duration: 4000,
     origin: 'right',
     distance: '-20px',
     reset: "true",
-    mobile: "false"
+    mobile: false
 });
 sr.reveal('.Seccion3', {
     duration: 4000,
     origin: 'left',
     distance: '-20px',
     reset: "true",
-    mobile: "false"
+    mobile: false
 });
 
