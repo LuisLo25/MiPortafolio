968b63e4c1f4c157e479d651d1094ccef5303efd
LuisLo25 <94856298+LuisLo25@users.noreply.github.com>
2023-07-25 10:43:26 -0500

    Version 1.9

--- JAVASCRIPT/AnimacionScroll.js
+++ JAVASCRIPT/AnimacionScroll.js
@@ -1,30 +1,22 @@
-
 window.sr = ScrollReveal();
 sr.reveal('.NavBar', {
     duration: 4000,
-    origin: 'bottom',
-    distance: '-20px',
+    origin: 'top',
     reset: true,
     mobile: false
-});
+})
 sr.reveal('.Seccion1', {
     duration: 4000,
-    origin: 'bottom',
-    distance: '-20px',
-    reset: "true",
+    reset: true,
     mobile: false
 });
 sr.reveal('.Seccion2', {
     duration: 4000,
-    origin: 'right',
-    distance: '-20px',
-    reset: "true",
+    reset: true,
     mobile: false
 });
 sr.reveal('.Seccion3', {
     duration: 4000,
-    origin: 'left',
-    distance: '-20px',
     reset: "true",
     mobile: false
 });
