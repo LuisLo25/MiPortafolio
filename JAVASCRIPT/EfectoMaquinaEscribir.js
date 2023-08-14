8f9fa049a0fda6edfa8865c5885b0a821ce180c2
LuisLo25 <94856298+LuisLo25@users.noreply.github.com>
2023-08-14 15:29:47 -0500

    Version 3.0

--- JAVASCRIPT/EfectoMaquinaEscribir.js
+++ JAVASCRIPT/EfectoMaquinaEscribir.js
@@ -1,29 +1,27 @@
-const maquina=document.getElementById('Texto')
-const maquinaEscribir=(text='',tiempo=200,etiqueta='')=>{
-    let arrayCaracteres=text.split('')
-    etiqueta.innerHTML=''
-    let cont=0
-    let escribir=setInterval(function(){
-        etiqueta.innerHTML+=arrayCaracteres[cont]
+const maquina = document.getElementById('Texto')
+const maquinaEscribir = (text = '', tiempo = 200, etiqueta = '') => {
+    let arrayCaracteres = text.split('')
+    etiqueta.innerHTML = ''
+    let cont = 0
+    let escribir = setInterval(function () {
+        etiqueta.innerHTML += arrayCaracteres[cont]
         cont++
-        if(cont===arrayCaracteres.length)
-        {
-            cont=0
-            etiqueta.innerHTML=''
+        if (cont === arrayCaracteres.length) {
+            cont = 0
+            etiqueta.innerHTML = ''
         }
-    },tiempo)
+    }, tiempo)
 }
-const maquinaEscribir2=(text='',tiempo=200,etiqueta='')=>{
-    let arrayCaracteres=text.split('')
-    etiqueta.innerHTML=''
-    let cont=0
-    let escribir=setInterval(function(){
-        etiqueta.innerHTML+=arrayCaracteres[cont]
+const maquinaEscribir2 = (text = '', tiempo = 200, etiqueta = '') => {
+    let arrayCaracteres = text.split('')
+    etiqueta.innerHTML = ''
+    let cont = 0
+    let escribir = setInterval(function () {
+        etiqueta.innerHTML += arrayCaracteres[cont]
         cont++
-        if(cont===arrayCaracteres.length)
-        {
+        if (cont === arrayCaracteres.length) {
             clearInterval(escribir)
         }
-    },tiempo)
+    }, tiempo)
 }
-maquinaEscribir("Desarrollador Junior Front-End ",300,maquina)
+maquinaEscribir("Egresado de Ing. de Sistemas Computacionales ", 300, maquina)
